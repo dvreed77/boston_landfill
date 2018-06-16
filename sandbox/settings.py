4c215e6434d70da9f1297693bd58be14a798e661
Dave Reed <dave.reed@tr.com>
2018-06-15 20:26:45 -0400

    Updates

--- sandbox/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-
-CROP = [235, 235]
-
-THRESHOLD = 0.23156393410941339
